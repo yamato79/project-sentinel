51576bec2da8384b1da7c3bf2689de36b524e195
yamato79 <yamato.takato@gmail.com>
2024-06-29 17:35:12 +0800

    Update reload interval to 60 seconds.

--- resources/js/pages/panel/websites/edit/summary.vue
+++ resources/js/pages/panel/websites/edit/summary.vue
@@ -60,9 +60,9 @@ const fetchUptimeFeed = () => {
     ]});
 };
 
-// Function to start reloading uptimeFeed every 30 seconds
+// Function to start reloading uptimeFeed every 60 seconds
 const startReloading = () => {
-    intervalId.value = setInterval(fetchUptimeFeed, 30000); // 30 seconds
+    intervalId.value = setInterval(fetchUptimeFeed, 60000); // 60 seconds
 };
 
 // Hook to start interval on component mount
