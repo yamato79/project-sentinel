af15ba118e1e9457a0a2bd8c32dd8a97aefa844d
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-07-01 02:08:56 +0000

    Update styling for checkbox component.

--- resources/js/components/form/form-checkbox.vue
+++ resources/js/components/form/form-checkbox.vue
@@ -5,7 +5,7 @@ const modelValue = defineModel();
 <template>
     <input
         type="checkbox"
-        class="v-form-input w-5 h-5 border-gray-200 rounded-sm focus:ring-0 focus:outline-none disabled:opacity-50 disabled:cursor-not-allowed"
+        class="v-form-input w-5 h-5 rounded border-gray-200 text-primary-600 focus:border-primary-600 focus:ring-primary-600 disabled:opacity-50 disabled:cursor-not-allowed"
         v-model="modelValue"
     />
 </template>
