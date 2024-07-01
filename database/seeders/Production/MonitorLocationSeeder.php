4c838108f700ff18bfd28fe5ecb5ef303f54ac87
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-07-01 02:09:25 +0000

    Set monitors to be inactive by default.

--- database/seeders/Production/MonitorLocationSeeder.php
+++ database/seeders/Production/MonitorLocationSeeder.php
@@ -18,7 +18,7 @@ class MonitorLocationSeeder extends Seeder
             ['name' => 'Africa',        'slug' => 'AF', 'agent_url' => "https://agent-af.{$monitorDomain}/api/monitors", 'is_active' => false, 'description' => 'Agent(s) found within the african region'],
             ['name' => 'Antarctica',    'slug' => 'AN', 'agent_url' => "https://agent-an.{$monitorDomain}/api/monitors", 'is_active' => false, 'description' => 'Agent(s) found within the antarctic region'],
             ['name' => 'Asia',          'slug' => 'AS', 'agent_url' => "https://agent-as.{$monitorDomain}/api/monitors", 'is_active' => false, 'description' => 'Agent(s) found within the asian region'],
-            ['name' => 'Europe',        'slug' => 'EU', 'agent_url' => "https://agent-eu.{$monitorDomain}/api/monitors", 'is_active' => true,  'description' => 'Agent(s) found within the european region'],
+            ['name' => 'Europe',        'slug' => 'EU', 'agent_url' => "https://agent-eu.{$monitorDomain}/api/monitors", 'is_active' => false, 'description' => 'Agent(s) found within the european region'],
             ['name' => 'North America', 'slug' => 'NA', 'agent_url' => "https://agent-na.{$monitorDomain}/api/monitors", 'is_active' => false, 'description' => 'Agent(s) found within the north american region'],
             ['name' => 'Oceania',       'slug' => 'OC', 'agent_url' => "https://agent-oc.{$monitorDomain}/api/monitors", 'is_active' => false, 'description' => 'Agent(s) found within the oceanian region'],
             ['name' => 'South America', 'slug' => 'SA', 'agent_url' => "https://agent-sa.{$monitorDomain}/api/monitors", 'is_active' => false, 'description' => 'Agent(s) found within the south american region'],
