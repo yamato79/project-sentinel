245c9d51ef8cd6b716327c416f69b5f7100a46e3
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-07-04 16:08:05 +0000

    Update theme colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -64,7 +64,7 @@ export default {
                 },
         
                 success: {
-                    ...colors.emerald,
+                    ...colors.green,
                 },
         
                 warning: {
@@ -72,7 +72,7 @@ export default {
                 },
         
                 danger: {
-                    ...colors.rose,
+                    ...colors.red,
                 },
             },
         },
