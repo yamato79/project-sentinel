8c07aa08b9a9d3b784a13820e19eacabbcd1c8be
Jonathan Tordesillas <yamato.takato@gmail.com>
2024-07-06 10:30:55 +0000

    remove dashed border.

--- resources/js/components/section-grid.vue
+++ resources/js/components/section-grid.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="v-section-grid grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3 first:border-t-0 border-t-2 border-gray-200 border-dashed pt-12 first:pt-0">
+    <div class="v-section-grid grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3 first:border-t-0 border-t-2 border-gray-200 pt-12 first:pt-0">
         <slot></slot>
     </div>
 </template>
